5913d57755de80490a09e8b7d1c1c42b23ca16c8
Tm9Yoo <fagottinobiesse1@gmail.com>
2026-07-08 00:24:03 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,9 +24,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "Segui Tm9Yoo dappertutto ed entra a far parte della sua community ds!",
         // Activity type number (0 = Playing).
-        type: 0,
+        type: 3,
       },
     ],
   },
